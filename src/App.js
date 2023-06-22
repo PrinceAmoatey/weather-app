f01598de41a5736a74b6ee56dcde8c6b2bad50da
Prince Cobby Amoatey <princecobbyamoatey@PAC.local>
2023-06-22 15:46:35 +0000

    location-section

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April","May", "June","July","August","September","October","November","December"];
+    let days = ["Sundays", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let date = d.getDate();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
     <main>
     <div className="search-box">
     <input 
     type="text" className="search-bar" placeholder="Search..."/>
+    </div>
+
+    <div className="location-box" >
+      <div className="location">Somanya, GH</div>
+      <div className="date">{dateBuilder(new Date())}</div>
     </div>
     </main>
      
