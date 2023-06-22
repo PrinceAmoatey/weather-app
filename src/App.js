cb560222ea2c89254204fa47910e576bbf160ff0
Prince Cobby Amoatey <princecobbyamoatey@PAC.local>
2023-06-22 14:15:24 +0000

    search-box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+
+const api = {
+  key: "dcba13edf62c3d0871b3ee90e21cc1a8",
+  base: "https://home.openweathermap.org/api_keys",
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <main>
+    <div className="search-box">
+    <input 
+    type="text" className="search-bar" placeholder="Search..."/>
+    </div>
+    </main>
+     
+    
     </div>
   );
 }
